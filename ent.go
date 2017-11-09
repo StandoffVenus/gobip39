1dbdfb6e7cb9f9140ad486504674e3d69deaf2a8
Liam Mueller <liamsimulate@gmail.com>
2017-11-09 16:17:22 -0600

    (This commit should have been first, woops) Fixed bug where (by my misreading) Entropy's Data was initialized to the SHA256 digest of its real data. Added method GetEntropyFromBytes that returns an instance of Entropy where Data is initialized to the passed byte array, along with correctly determined Size. Entropy is now an exported type. GenerateChecksum is now the method that SHA256's Entropy's Data (correctly so). Comment and error message revisions.

--- ent.go
+++ ent.go
@@ -8,6 +8,7 @@ import (
 	"crypto/rand"
 	"github.com/32bitkid/bitreader"
 	"bytes"
+	"fmt"
 )
 
 const (
@@ -27,48 +28,78 @@ func (err entropyError) Error() string {
 }
 
 // Type to wrap entropy-related methods
-type entropy struct {
+type Entropy struct {
 	Size uint16
 	Data []byte
 }
 
 // Generate entropy with specified amount of bits.
-// Returns the generated entropy and potential error.
 // GenerateEntropy will return an error if the passed size
-// does not conform to specification limits.
-func GenerateEntropy(size uint16) (entropy, error) {
+// does not conform to specification limits:
+// 1) Size of Entropy (in bits) is within domain [128, 256],
+// and 2) Size of Entropy is not a multiple of 32.
+// In the case of error, the returned Entropy will be
+// in an invalid state.
+func GenerateEntropy(size uint16) (Entropy, error) {
 	// If size is outside allowed domain
 	if (size < MinimumEntropySize || size > MaximumEntropySize) {
-		return entropy{}, entropyError{Message: "Size of entropy is out of range [128, 256]."}
+		return Entropy{}, entropyError{Message: "Size of entropy is out of domain [128, 256]."}
 	}
 
 	// If size is not divisible by 32
 	if (size % 32 != 0) {
-		return entropy{}, entropyError{Message: "Size of entropy is not divisible by 32."}
+		return Entropy{}, entropyError{Message: "Size of entropy is not a multiple of 32."}
 	}
 
 	// Read random bytes
 	randomBytes := make([]byte, size / 8)
 	rand.Read(randomBytes)
 
-	// Make SHA256 hash and read random data into it
-	shaHash := sha256.New()
-	shaHash.Write(randomBytes)
+	// Return Entropy from these random bytes
+	return GetEntropyFromBytes(randomBytes)
+}
 
-	return entropy{size, shaHash.Sum(nil)}, nil
+// Generate Entropy from hex data.
+// An error is returned if the size (in bits) of the data
+// is outside the domain of valid entropy size, or if the
+// length of the data is not a multiple of 32, in which
+// the Entropy returned is in an invalid state.
+func GetEntropyFromBytes(data []byte) (Entropy, error) {
+	// Early check to prevent errors from wrap around on uint16 conversion
+	if (int(^uint16(0)) < len(data)) {
+		return Entropy{}, entropyError{Message: "Length of data (in bits) was outside of domain [128, 256]."}
+	}
+
+	dataLength := uint16(len(data)) * 8
+
+	if (dataLength > MaximumEntropySize || dataLength < MinimumEntropySize) {
+		return Entropy{}, entropyError{Message: "Length of data (in bits) was outside of domain [128, 256]."}
+	}
+
+	if (dataLength % 32 != 0) {
+		return Entropy{}, entropyError{Message: "Length of data (in bits) was not a multiple of 32."}
+	}
+
+	return Entropy{Size: dataLength, Data: data}, nil
 }
 
 // Generate checksum will return a checksum of the given entropy
 // via the specification, returning said checksum as a byte. An empty
 // byte and error will be returned if an error is encountered while
 // trying to read the checksum.
-func (ent entropy) GenerateChecksum() (byte, error) {
-	// Convert entropy data to a Reader
-	byteReader := bytes.NewReader(ent.Data)
+func (ent Entropy) GenerateChecksum() (byte, error) {
+	// We must SHA the data for the checksum
+	shaHash := sha256.New()
+	shaHash.Write(ent.Data)
+
+	// Convert data to a Reader
+	byteReader := bytes.NewReader(shaHash.Sum(nil))
 
 	bitReader := bitreader.NewBitReader(byteReader)
 
-	// Read the first (entropy's bits / 32) bits of the entropy data
+	// Read the first (entropy's bits / 32) bits of the SHA256 digest
+	fmt.Println(ent.Size)
+
 	checksum, err := bitReader.Read32(uint(ent.Size / 32))
 
 	if (err != nil) { return 0, entropyError{ Message: err.Error() }}
